1b95b1054e5986093d903829191b184a1a82d140
believer <18763821496@163.com>
2022-12-19 17:23:33 +0800

    151

--- src/leetcode/151.cpp
+++ src/leetcode/151.cpp
@@ -14,17 +14,44 @@ string removeSpace(string& s) {
 				s[slow++] = s[fast];		
 		}
 	}
-	s.resize(slow);
+	if (s[slow - 1] == ' ')
+		s.resize(slow - 1);
+	else
+		s.resize(slow);
 	return s;
 }
 
+string reverse(string& s, int start, int end) {
+	for (start; start < end; start++, end--) {
+		swap(s[start], s[end]);
+	}
+	return s;
+}
 
+string reverseWord(string& s) {
+	removeSpace(s);// 去除多余空格
+	reverse(s, 0, s.size() - 1); // 整个字符串翻转
+
+	int slow = 0;
+	for (int fast = 0; fast <= s.size(); fast++) {
+		if (s[fast] == ' ' || fast == s.size()) {
+			reverse(s, slow, fast - 1);
+			slow = fast + 1;
+		}
+	}
+	return s;
+}
 
 int main() {
 
-	string str1 = " abb asg   k   a sksd j   ";
-	string str2 = removeSpace(str1);
+	string str1 = " the sky is blue ";
+	/*string str2 = removeSpace(str1);
 	cout << str2 << endl;
+	string str3 = reverse(str2, 0, str2.size() - 1);
+	cout << str3 << endl;
+	string str4 = reverseWord(str3);
+	cout << str4 << endl;*/
+	cout << reverseWord(str1) << endl;
 
 	system("pause");
 	return 0;
